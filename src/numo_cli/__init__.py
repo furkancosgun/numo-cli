3b9500fc1a30e4a23f7b377952d8043f44f42f94
Furkan COSGUN <furkan51cosgun@gmail.com>
2024-12-26 20:00:52 +0300

    Bump version to 0.1.6 in Numo CLI

--- src/numo_cli/__init__.py
+++ src/numo_cli/__init__.py
@@ -2,7 +2,7 @@
 Numo CLI - A powerful command-line interface for calculations, unit conversions, and translations.
 """
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __author__ = "Furkan Cosgun"
 __email__ = "furkan51cosgun@gmail.com"
 
